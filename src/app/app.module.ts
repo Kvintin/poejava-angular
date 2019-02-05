77f65152b431a1cf3d2fc84ecd7271b9e9fc19f0
Vincent Letourneau <vincent@nanoninja.com>
2019-02-05 16:56:39 +0100

    Ajout du formulaire job

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {
-  NgbModule
+  NgbModule, NgbDatepicker, NgbDatepickerModule
 } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -29,6 +29,7 @@ import { JobListComponent } from './components/job-list/job-list.component';
     BrowserModule,
     HttpClientModule,
     FormsModule,
+    ReactiveFormsModule,
     NgbModule,
     AppRoutingModule
   ],
--- src/app/components/job-add/job-add.component.ts
+++ src/app/components/job-add/job-add.component.ts
@@ -1,4 +1,8 @@
+import { ContractType } from './../../models/contract-type.model';
+import { JobService } from './../../services/job.service';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-job-add',
@@ -6,10 +10,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./job-add.component.css']
 })
 export class JobAddComponent implements OnInit {
+  contractTypes: ContractType[] = [
+    { id: 1,  label: 'Stage' },
+    { id: 2,  label: 'Interim' },
+    { id: 3,  label: 'CDD' },
+    { id: 4,  label: 'CDI' },
+    { id: 5,  label: 'Indépendant' },
+  ];
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(
+    private router: Router,
+    private formBuilder: FormBuilder,
+    private jobService: JobService
+  ) {}
 
   ngOnInit() {
+    this.form = this.formBuilder.group({
+      title: ['', [Validators.required, Validators.pattern(/^[a-z\s\-]{2,10}$/i)]],
+      company: '',
+      city: '',
+      zipcode: '',
+      description: '',
+      contractType: null,
+      startDate: new Date(),
+      publishedDate: new Date()
+    });
   }
 
+  onSave() {
+    if (this.form.valid) {
+      console.log(this.form.value);
+    } else {
+      console.log("c'est pas valid");
+    }
+  }
+
+  get title() {
+    return this.form.get('title');
+  }
 }
